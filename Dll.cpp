0b3cdcbc648499d2a67feea18fda122549c55346
N00byEdge <me@hannesbredberg.se>
2017-08-27 04:43:48 +0200

    More linux compat

--- Dll.cpp
+++ Dll.cpp
@@ -1,11 +1,21 @@
 #include <BWAPI.h>
-#ifdef WIN32
+
+#ifdef WIN32 // Windows
 
 #include <Windows.h>
+#define DLLEXPORT __declspec(dllexport)
+
+#else // Not windows
+
+#define DLLEXPORT
+
+#endif
 
 #include "Neohuman.h"
 
-extern "C" __declspec(dllexport) void gameInit(BWAPI::Game* game) { BWAPI::BroodwarPtr = game; }
+extern "C" DLLEXPORT void gameInit(BWAPI::Game* game) { BWAPI::BroodwarPtr = game; }
+
+#ifdef WIN32
 BOOL APIENTRY DllMain( HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved ) {
   switch (ul_reason_for_call) {
   case DLL_PROCESS_ATTACH:
@@ -15,9 +25,8 @@ BOOL APIENTRY DllMain( HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
   }
   return TRUE;
 }
+#endif
 
-extern "C" __declspec(dllexport) BWAPI::AIModule* newAIModule() {
+extern "C" DLLEXPORT BWAPI::AIModule* newAIModule() {
 	return neoInstance = new Neohuman();
 }
-
-#endif
