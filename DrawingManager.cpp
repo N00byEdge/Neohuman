1c98a143973cd12f97b70a9d098ab13906f723bf
N00byEdge <me@hannesbredberg.se>
2017-09-19 00:33:17 +0200

    Send Nukes

--- DrawingManager.cpp
+++ DrawingManager.cpp
@@ -12,6 +12,17 @@
 #include "BWAPI.h"
 #include "bwem.h"
 
+const std::vector <std::string> botAsciiArt = {
+	" ad88888ba                                     88     888b      88               88",
+	"d8\"     \"8b                                    88     8888b     88               88",
+	"Y8,                                            88     88 `8b    88               88",
+	"`Y8aaaaa,     ,adPPYba,  8b,dPPYba,    ,adPPYb,88     88  `8b   88  88       88  88   ,d8   ,adPPYba,  ,adPPYba,",
+	"  `\"\"\"\"\"8b,  a8P_____88  88P'   `\"8a  a8\"    `Y88     88   `8b  88  88       88  88 ,a8\"   a8P_____88  I8[    \"\"",
+	"        `8b  8PP\"\"\"\"\"\"\"  88       88  8b       88     88    `8b 88  88       88  8888[     8PP\"\"\"\"\"\"\"   `\"Y8ba,",
+	"Y8a     a8P  \"8b,   ,aa  88       88  \"8a,   ,d88     88     `8888  \"8a,   ,a88  88`\"Yba,  \"8b,   ,aa  aa    ]8I",
+	" \"Y88888P\"    `\"Ybbd8\"'  88       88   `\"8bbdP\"Y8     88      `888   `\"YbbdP'Y8  88   `Y8a  `\"Ybbd8\"'  `\"YbbdP\"'"
+};
+
 const std::vector <int> columnXStart = { 0, 160, 280, 430, 510, 175 };
 const std::vector <int> columnYStart = { 0, 0, 0, 200, 16, 312 };
 
@@ -45,6 +56,31 @@ const inline static void drawUnitBox(BWAPI::Position pos, BWAPI::UnitType type, 
 
 const inline static void drawBuildingBox(BWAPI::TilePosition pos, BWAPI::UnitType type, BWAPI::Color c) {
 	BWAPI::Broodwar->drawBoxMap((BWAPI::Position)pos, (BWAPI::Position)pos + (BWAPI::Position)type.tileSize(), c);
+}
+
+const inline static void drawBotName() {
+	constexpr int charWidth = 5;
+	constexpr int charHeight = 8;
+	constexpr int boxWidth = 111 * charWidth;
+	constexpr int boxHeight = 8 * charHeight;
+#ifdef SSCAIT
+	constexpr int screenWidth = 1920;
+#else
+	constexpr int screenWidth = 640;
+#endif
+
+	BWAPI::Broodwar->setTextSize(BWAPI::Text::Size::Small);
+	BWAPI::Broodwar->drawBoxScreen(screenWidth - boxWidth, 0, screenWidth, boxHeight, BWAPI::Colors::Black, true);
+	int ypos = 0;
+	for (auto &s : botAsciiArt) {
+		int xpos = 0;
+		for (auto &c : s) {
+			BWAPI::Broodwar->drawTextScreen(screenWidth - boxWidth + xpos, ypos, "%c%c", BWAPI::Text::White, c);
+			xpos += charWidth;
+		}
+		ypos += charHeight;
+	}
+	BWAPI::Broodwar->setTextSize(BWAPI::Text::Size::Default);
 }
 
 const inline static BWAPI::Color healthColor(int health, int maxHealth) {
@@ -111,7 +147,7 @@ namespace Neolib {
 			BWAPI::Broodwar->drawTextScreen(columnXStart[1], getNextColumnY(nextColumnY[1]), "Nukes: %c%u Armed %c%u Arming %c%u Unarmed", BWAPI::Text::BrightRed, unitManager.getNumArmedSilos(), BWAPI::Text::Orange, unitManager.getNumArmingSilos(), BWAPI::Text::Yellow, unitManager.getNumUnarmedSilos());
 			BWAPI::Broodwar->drawTextScreen(columnXStart[1], getNextColumnY(nextColumnY[1]), "%c%d%c nukes launched", BWAPI::Text::Green, unitManager.getLaunchedNukeCount(), BWAPI::Text::Default);
 			//BWAPI::Broodwar->drawTextScreen(columnXStart[1], getNextColumnY(nextColumnY[1]), "I have %d barracks!", unitManager.countFriendly(BWAPI::UnitTypes::Terran_Barracks));
-			//BWAPI::Broodwar->drawTextScreen(columnXStart[1], getNextColumnY(nextColumnY[1]), "I have %d APM!", BWAPI::Broodwar->getAPM());
+			BWAPI::Broodwar->drawTextScreen(columnXStart[1], getNextColumnY(nextColumnY[1]), "I have %d APM!", BWAPI::Broodwar->getAPM());
 			BWAPI::Broodwar->drawTextScreen(columnXStart[1], getNextColumnY(nextColumnY[1]), "Income: %c%d %c%d", BWAPI::Text::Blue, resourceManager.getMinuteApproxIncome().minerals, BWAPI::Text::Green, resourceManager.getMinuteApproxIncome().gas, BWAPI::Text::Default, baseManager.getHomelessWorkers().size());
 			BWAPI::Broodwar->drawTextScreen(columnXStart[1], getNextColumnY(nextColumnY[1]), "%u Idle workers", baseManager.getHomelessWorkers().size());
 
